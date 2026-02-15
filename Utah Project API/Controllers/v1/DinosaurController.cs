25a80eb65042542f1eb0711c9739e3da47287cd4
Stijn Wellink <wellinkstijn@gmail.com>
2026-02-15 20:01:51 +0100

    Add Dinosaur API controller, service and exception
    Add initial Dinosaur API components: a DinosaurController with a GET endpoint to return all dinosaurs; a NotFoundException custom exception class; and a DinosaurService implementing IDinosaurService that provides GetAllDinosaurs and GetDinosaurById (throws NotFoundException if missing) and includes stubbed methods (throwing NotImplementedException) for other dinosaur operations. Service uses ApplicationDbContext/EF Core to access Dinosaurs.

---
+++ Utah Project API/Controllers/v1/DinosaurController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Utah_Project_API.Interfaces;
+using Utah_Project_API.Models.Dinosaur;
+
+namespace Utah_Project_API.Controllers.v1;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+[Produces("application/json")]
+public class DinosaurController (IDinosaurService dinosaurServiceService) : ControllerBase
+{
+    /// <summary>
+    /// get all dinosaurs
+    /// </summary>
+    /// <returns>list of dinosaurs</returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(List<Dinosaur>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<Dinosaur>>> GetAllDinosaurs()
+    {
+        List<Dinosaur> dinosaurs = await dinosaurServiceService.GetAllDinosaurs();
+        return Ok(dinosaurs);
+    }
+    
+}
---
+++ Utah Project API/Services/DinosaurService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Utah_Project_API.Data;
+using Utah_Project_API.Exceptions;
+using Utah_Project_API.Interfaces;
+using Utah_Project_API.Models.Dinosaur;
+
+namespace Utah_Project_API.Services;
+
+public class DinosaurService(
+    ApplicationDbContext context
+    ) : IDinosaurService
+{
+    public async Task<List<Dinosaur>> GetAllDinosaurs()
+    {
+        List<Dinosaur> dinosaurs = await context.Dinosaurs.ToListAsync();
+        
+        return dinosaurs;
+    }
+
+    public async Task<Dinosaur> GetDinosaurById(int dinoCode)
+    {
+        Dinosaur? dinosaur = await context.Dinosaurs.FirstOrDefaultAsync(d => d.DinoCode == dinoCode);
+        if (dinosaur == null)
+        {
+            throw new NotFoundException("Dinosaur not found");
+        }
+
+        return dinosaur;
+    }
+
+    public async Task<Dinosaur> GetAllDinosaursUser(string userId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> DeleteDinosaur(string userId, int dinoCode)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> AddBehaviourToDinosaur(int dinoCode, int behaviourId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> RemoveBehaviourFromDinosaur(int dinoCode, int behaviourId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> AddNestToDinosaur(int dinoCode, int nestingId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> RemoveNestFromDinosaur(int dinoCode, int nestingId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> AddMutationToDinosaur(int dinoCode, int mutationId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Dinosaur> RemoveMutationFromDinosaur(int dinoCode, int mutationId)
+    {
+        throw new System.NotImplementedException();
+    }
+}
