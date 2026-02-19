ceb2fa64d72cb737cc4fd8726e66587e9a038787
Stijn Wellink <wellinkstijn@gmail.com>
2026-02-19 22:53:51 +0100

    gives gender descriptions

--- Utah Project API/Enums/Gender.cs
+++ Utah Project API/Enums/Gender.cs
@@ -2,7 +2,18 @@
 
 public enum Gender
 {
+    /// <summary>
+    /// the dino who lays the egg
+    /// </summary>
     EggLayer,
+    
+    /// <summary>
+    /// the dino who gives the egg
+    /// </summary>
     EggGiver,
+    
+    /// <summary>
+    /// Dino who does both
+    /// </summary>
     Asexual
 }
