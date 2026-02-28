6cf5f9d1e15187a95384869ea60cadeade540ca0
Stijn Wellink <wellinkstijn@gmail.com>
2026-02-28 23:15:07 +0100

    moved to separate repo

--- frontend/src/types/css-modules.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.module.css" {
-    const classes: Readonly<Record<string, string>>;
-    export default classes;
-}
-
-declare module "*.module.scss" {
-    const classes: Readonly<Record<string, string>>;
-    export default classes;
-}
